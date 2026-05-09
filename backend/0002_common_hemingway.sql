97058f5b6ae75c96598f268705163b3b46a8d628
wellingtonmatheus2019-bit <wellingtonmatheus2019@gmail.com>
2026-05-09 11:25:25 -0300

    Delete backend directory

--- backend/0002_common_hemingway.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `analytics` MODIFY COLUMN `engagementRate` decimal(5,2) NOT NULL DEFAULT '0';--> statement-breakpoint
-ALTER TABLE `analytics` MODIFY COLUMN `conversionRate` decimal(5,2) NOT NULL DEFAULT '0';--> statement-breakpoint
-ALTER TABLE `instagramAccounts` MODIFY COLUMN `engagement` decimal(5,2) NOT NULL DEFAULT '0';
